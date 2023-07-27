620c8f5ce1646ddfe4c5ed887d7181b3b97f12a5
Poliih <polianarodriguesds@gmail.com>
2023-07-26 21:38:22 -0300

    First

--- sitema_bancario.py
+++ sitema_bancario.py
@@ -17,19 +17,19 @@ while True:
     opcao = input(menu)
 
     if opcao == "d":
-        deposito = int(input("Valor do depósito: "))
+        deposito = float(input("Valor do depósito: "))
         if deposito > 0:
-            saldo = saldo + deposito
-            extrato.append(f"Depósito: +{deposito}")
+            saldo += deposito
+            extrato.append(f"Depósito: +R$ {deposito:.2f}")
         else:
             print("Não é possível depositar esse valor")
 
     elif opcao == "s":
-        saque = int(input("Valor do saque: "))
+        saque = float(input("Valor do saque: "))
         if numero_saques < LIMITE_SAQUES and saque <= 500:
             if saque <= saldo:
                 saldo = saldo - saque
-                extrato.append(f"Saque: -{saque}")
+                extrato.append(f"Saque: -R$ {saque:.2f}")
                 numero_saques += 1
             else:
                 print("Você não possui saldo suficiente.")
@@ -43,7 +43,7 @@ while True:
         print("\nExtrato:")
         for item in extrato:
             print(item)
-        print("Saldo Atual:", saldo, "\n")
+        print("Saldo Atual: R$ {:.2f}\n".format(saldo))
 
     elif opcao == "q":
         break
